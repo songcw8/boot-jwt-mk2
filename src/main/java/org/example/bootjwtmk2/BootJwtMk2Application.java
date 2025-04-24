8b37c4d14983cfec125d53fc0d8d34b37f2cf533
songcw8 <ben990158@gmail.com>
2025-04-24 10:11:24 +0900

    Initial commit

---
+++ src/main/java/org/example/bootjwtmk2/BootJwtMk2Application.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtmk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJwtMk2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJwtMk2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootjwtmk2/BootJwtMk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtmk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJwtMk2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
