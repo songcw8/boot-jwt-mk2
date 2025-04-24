8135e9f002e30d8ddbb5c4de331b903fbf0f880b
songcw8 <ben990158@gmail.com>
2025-04-24 11:47:15 +0900

    Filter 만들기

---
+++ src/main/java/org/example/bootjwtmk2/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package org.example.bootjwtmk2.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, resp);
+    }
+}
